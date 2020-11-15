46782cdc5ca7a9153a9a460346335135e587e6d6
Lorenz Stechauner <lorenz.stechauner@gmail.com>
2020-11-15 11:43:34 +0100

    Sessions

--- sessions.py
+++ sessions.py
@@ -29,9 +29,10 @@ def is_ip_address(hostname: str) -> bool:
 
 
 def get_crawler_id(session) -> str:
-    hostname = session['history'][0]['host']
+    hostname = session['history'][-1]['host']
     if hostname is None or hostname == '':
-        hostname = session['history'][0]['address']
+        hostname = session['history'][-1]['address']
+    print(hostname)
     if is_ip_address(hostname):
         p = hostname.rfind('.')
         tld = hostname[:p]
@@ -46,7 +47,7 @@ def get_bot_session_id(session) -> str:
     for sess_id, sess in virtual_sessions.items():
         if get_crawler_id(sess) == cid:
             for nr, req in sess['history'].items():
-                if abs(req['timestamp'] - session['history'][0]['timestamp']) < 60 * 60:
+                if abs(req['timestamp'] - session['history'][-1]['timestamp']) < 60 * 60:
                     return sess_id
     return session['sess_id']
 
